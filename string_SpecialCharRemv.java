0dfdf885f1ff0d86a908a47c3bad195c51076734
Yash Tank <62990171+yashtank09@users.noreply.github.com>
2022-04-29 18:35:31 +0530

    Using Regex removed all special char from string

---
+++ string_SpecialCharRemv.java
@@ -0,0 +1,35 @@
+import java.util.Arrays;
+import java.util.stream.Collector;
+import java.util.stream.Collectors;
+
+/**
+ * string_SpecialCharRemv
+ */
+
+public class string_SpecialCharRemv {
+
+    public static void main(String[] args) {
+        String s = "Hello, Yash!. This is String Special Char Remove.";
+        
+        String splits[] = s.split("[!,?._'@ ]");
+        splits = Arrays.asList(splits).stream().filter(str -> !str.isEmpty()).collect(Collectors.toList()).toArray(new String[0]);
+        System.out.println(splits.length);
+        for(String z : splits)
+            System.out.println(z);
+        
+        // Scanner scan = new Scanner(System.in);
+        // String s = scan.nextLine();
+        // scan.close();
+
+        // String splits[] = s.split("[!,?._'@ ]");
+    	// splits = Arrays.asList(splits).stream().filter(str -> !str.isEmpty()).collect(Collectors.toList()).toArray(new String[0]);
+    	// System.out.println(splits.length);
+    	// for(String z : splits)
+    		// System.out.println(z);
+        
+        // String spc = st.replaceAll("\\s+|[\\-\\+\\$\\?\\.@&].*", "\n");
+        // System.out.println(spc.length());
+        // st = st.replaceAll("[^a-zA-Z0-9]+", "\n");
+        // System.out.println(st);
+    }
+}
