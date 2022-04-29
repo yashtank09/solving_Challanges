edbf2717245bcdcde7ef651b9a058d9324c04a47
Yash Tank <62990171+yashtank09@users.noreply.github.com>
2022-04-29 15:44:08 +0530

    Watcched Second aproach

--- string_Anagrams.java
+++ string_Anagrams.java
@@ -8,7 +8,6 @@ import java.util.*;
  */
 
 public class string_Anagrams {
-    
     static boolean isAnagram(String a, String b) {
         // Complete the function
         
@@ -29,8 +28,17 @@ public class string_Anagrams {
         char[] str1 = a.toCharArray();
         char[] str2 = a.toCharArray();
 
-        Arrays.sort(str1);
-        Arrays.sort(str2);
+        // int letters[] = new int[26];
+
+        // for(char x : str1)
+        //     letters[x - 'a']++;
+        
+        // for(int i = 0; i < MAX_CHAR; i++)
+        //     for(int j = 0; j < letters[i]; j++)
+                
+
+        // Arrays.sort(str1);
+        // Arrays.sort(str2);
 
         for(int i = 0; i < n1; i++)
             if(str1[i] == str2[i])
@@ -49,4 +57,37 @@ public class string_Anagrams {
         boolean ret = isAnagram(a, b);
         System.out.println( (ret) ? "Anagrams" : "Not Anagrams" );
     }
+    /*
+    Second Aproach:
+
+    static boolean isAnagram(String a, String b){
+        String s1 = a;
+        String s2 = b;
+        s1=s1.toLowerCase();
+        s2=s2.toLowerCase();
+        
+        if(s1.length()==s2.length())
+        {
+            int[] arr = new int[256];
+            int[] brr = new int[256];
+            
+            for (int i = 0; i < s1.length(); i++) {
+                arr[(int) s1.charAt(i)] += 1;
+                brr[(int) s2.charAt(i)] += 1;
+            }
+            
+            for (int i = 0; i < 256; i++) {
+                if (arr[i] != brr[i])
+                    return false;
+            }
+            
+            return true;
+        }
+        else
+        {
+            return false;
+        }
+    }
+    */
 }
+
